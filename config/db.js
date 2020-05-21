dcaf88a92db931bc36976b9beb25449c9cb79ecc
Brian <brian.holland.pr@gmail.com>
2020-05-21 03:57:02 -0700

    fixed deprec warning for mongoose deletions

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
         await mongoose.connect(db, {
             useNewUrlParser: true,
             useCreateIndex: true,
-            useFindAndModify: true,
+            useFindAndModify: false,
             useUnifiedTopology: true,
         });
 
