dbd9b866f852933663bf8a05cf1f8e4c129a0394
Brian <brian.holland.pr@gmail.com>
2020-05-21 00:59:13 -0700

    added auth validation to login with jwt

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+const User = require("../models/User");
 
 //Router    GET api/auth
 //Descr     Get logged in user
@@ -11,8 +17,51 @@ router.get("/", (req, res) => {
 //Router    POST api/auth
 //Descr     Auth user and get token
 //Access    Public
-router.post("/", (req, res) => {
-    res.send("Log in user");
-});
+router.post(
+    "/",
+    [
+        check("email", "Please include a valid email").isEmail(),
+        check("password", "Password is required").exists(),
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { email, password } = req.body;
+
+        try {
+            let user = await User.findOne({ email });
+            //if no email for user exists, send message
+            if (!user) {
+                return res.status(400).json({ msg: "Invalid Credentials" });
+            }
+            //check if passwords match
+            const isMatch = await bcrypt.compare(password, user.password);
+            //if passwords dont match, send message
+            if (!isMatch) {
+                return res.status(400).json({ msg: "Invalid Credentials" });
+            }
+            //create object to send in webtoken
+            const payload = {
+                user: {
+                    id: user.id,
+                },
+            };
+
+            //sign webtoken using jwtsecret from confog file
+            jwt.sign(
+                payload,
+                config.get("jwtsecret"),
+                { expiresIn: 360000 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            );
+        } catch (err) {}
+    }
+);
 
 module.exports = router;
