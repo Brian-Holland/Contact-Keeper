640a44cd840cfc5485c0f872f688097544a6a565
Brian <brian.holland.pr@gmail.com>
2020-05-21 03:17:54 -0700

    added add contact route with auth and validation

--- routes/contacts.js
+++ routes/contacts.js
@@ -24,9 +24,35 @@ router.get("/", auth, async (req, res) => {
 //Route     POST api/contacts
 //Desc      Add new contact
 //Access    Private
-router.post("/", (req, res) => {
-    res.send("Add contact");
-});
+router.post(
+    "/",
+    [auth, [check("name", "Name is required").not().isEmpty()]],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { name, email, phone, type } = req.body;
+
+        try {
+            const newContact = new Contact({
+                name,
+                email,
+                phone,
+                type,
+                user: req.user.id,
+            });
+
+            const contact = await newContact.save();
+
+            res.json(contact);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send("Server Error");
+        }
+    }
+);
 
 //Route     PUT api/contacts
 //Desc      Update contact
