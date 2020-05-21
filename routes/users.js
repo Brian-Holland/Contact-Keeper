8ee3f4b8fb0edbc41f63f62a48432b401a05555e
Brian <brian.holland.pr@gmail.com>
2020-05-20 21:45:34 -0700

    added user creation, encrypting, and jsonwebtokens

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 const User = require("../models/User");
 
@@ -17,13 +20,58 @@ router.post(
             "Please enter a password with 6 or more characters"
         ).isLength({ min: 6 }),
     ],
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
+
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
 
-        res.send("passed checks");
+        const { name, email, password } = req.body;
+
+        try {
+            //attempt to find user in db by email
+            let user = await User.findOne({ email });
+
+            //if there is already a user...
+            if (user) {
+                //respond that user exists
+                return res.status(400).json({ msg: "User already exists" });
+            }
+
+            //otherwise create instance of new user using the name,email,password
+            user = new User({ name, email, password });
+
+            //generate salt for the password encryption
+            const salt = await bcrypt.genSalt(10);
+
+            //hash the password using salt
+            user.password = await bcrypt.hash(password, salt);
+
+            //save user to db
+            await user.save();
+
+            //create object to send in webtoken
+            const payload = {
+                user: {
+                    id: user.id,
+                },
+            };
+
+            //sign webtoken using jwtsecret from confog file
+            jwt.sign(
+                payload,
+                config.get("jwtsecret"),
+                { expiresIn: 360000 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            );
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send("Server Error");
+        }
     }
 );
 
