547f238affe8d54b8d6214863168dc786afad392
Brian <brian.holland.pr@gmail.com>
2020-05-21 03:49:26 -0700

    added delete contact route

--- routes/contacts.js
+++ routes/contacts.js
@@ -72,7 +72,7 @@ router.put("/:id", auth, async (req, res) => {
         if (!contact) return res.status(404).json({ msg: "Contact not found" });
 
         //make sure user owns contact
-        if (contact.user.toString !== req.user.id) {
+        if (contact.user.toString() !== req.user.id) {
             return res.status(401).json({ msg: "Not authorized" });
         }
         contact = await Contact.findByIdAndUpdate(
@@ -91,8 +91,23 @@ router.put("/:id", auth, async (req, res) => {
 //Route     DELETE api/contacts
 //Desc      Delete contact
 //Access    Private
-router.delete("/:id", (req, res) => {
-    res.send("Delete contact");
+router.delete("/:id", auth, async (req, res) => {
+    try {
+        let contact = await Contact.findById(req.params.id);
+
+        if (!contact) return res.status(404).json({ msg: "Contact not found" });
+
+        //make sure user owns contact
+        if (contact.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: "Not authorized" });
+        }
+        await Contact.findByIdAndRemove(req.params.id);
+
+        res.json({ msg: "Contact removed" });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
 });
 
 module.exports = router;
