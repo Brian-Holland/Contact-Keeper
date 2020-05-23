af5563e228d4b69ceacafa51ff655c4c25501f7c
Brian <brian.holland.pr@gmail.com>
2020-05-22 21:51:48 -0700

    finished styling contactitem cards

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const ContactItem = ({ contact }) => {
     const { id, name, email, phone, type } = contact;
@@ -7,6 +8,7 @@ const ContactItem = ({ contact }) => {
             <h3 className="text-primary text-left">
                 {name}{" "}
                 <span
+                    style={{ float: "right" }}
                     className={
                         "badge " +
                         (type === "professional"
@@ -17,8 +19,27 @@ const ContactItem = ({ contact }) => {
                     {type.charAt(0).toUpperCase() + type.slice(1)}
                 </span>
             </h3>
+            <ul className="list">
+                {email && (
+                    <li>
+                        <i className="fas fa-envelope-open" /> {email}
+                    </li>
+                )}
+                {phone && (
+                    <li>
+                        <i className="fas fa-phone" /> {phone}
+                    </li>
+                )}
+            </ul>
+            <p>
+                <button className="btn btn-dark btn-sm">Edit</button>
+                <button className="btn btn-danger btn-sm">Delete</button>
+            </p>
         </div>
     );
 };
 
+ContactItem.propTypes = {
+    contact: PropTypes.object.isRequired,
+};
 export default ContactItem;
