024de4dd1da768d94cad6513b19e1a43d643ab0a
Brian <brian.holland.pr@gmail.com>
2020-05-22 20:42:32 -0700

    styled contact items

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useContext } from "react";
 import ContactContext from "../../context/contact/contactContext";
+import ContactItem from "./ContactItem";
 
 const Contacts = () => {
     const contactContext = useContext(ContactContext);
@@ -8,7 +9,7 @@ const Contacts = () => {
     return (
         <Fragment>
             {contacts.map(contact => (
-                <h3>{contact.name}</h3>
+                <ContactItem key={contact.id} contact={contact} />
             ))}
         </Fragment>
     );
