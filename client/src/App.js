b303ac4acf86fed33c3d863bd16e84da34af446e
Brian <brian.holland.pr@gmail.com>
2020-05-24 16:07:28 -0700

    added login form and route

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 import ContactState from "./context/contact/ContactState";
 import AuthState from "./context/auth/AuthState";
@@ -25,6 +26,7 @@ const App = () => {
                                     path="/register"
                                     component={Register}
                                 />
+                                <Route exact path="/login" component={Login} />
                             </Switch>
                         </div>
                     </Fragment>
