297977fd31ccf3a29cf188ab0475423b69e6eeba
Brian <brian.holland.pr@gmail.com>
2020-05-23 22:10:44 -0700

    added filter

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,16 +1,39 @@
 import React, { Fragment, useContext } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import ContactContext from "../../context/contact/contactContext";
 import ContactItem from "./ContactItem";
 
 const Contacts = () => {
     const contactContext = useContext(ContactContext);
 
-    const { contacts } = contactContext;
+    const { contacts, filtered } = contactContext;
+
+    if (contacts.length === 0) {
+        return <h4>Please add a contact</h4>;
+    }
     return (
         <Fragment>
-            {contacts.map(contact => (
-                <ContactItem key={contact.id} contact={contact} />
-            ))}
+            <TransitionGroup>
+                {filtered !== null
+                    ? filtered.map(contact => (
+                          <CSSTransition
+                              key={contact.id}
+                              timeout={500}
+                              className="item"
+                          >
+                              <ContactItem contact={contact} />
+                          </CSSTransition>
+                      ))
+                    : contacts.map(contact => (
+                          <CSSTransition
+                              key={contact.id}
+                              timeout={500}
+                              className="item"
+                          >
+                              <ContactItem contact={contact} />
+                          </CSSTransition>
+                      ))}
+            </TransitionGroup>
         </Fragment>
     );
 };
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -38,6 +38,7 @@ const ContactState = props => {
             },
         ],
         current: null,
+        filtered: null,
     };
 
     const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -62,25 +63,35 @@ const ContactState = props => {
     const clearCurrent = () => {
         dispatch({ type: CLEAR_CURRENT });
     };
+
     //update contact
     const updateContact = contact => {
         dispatch({ type: UPDATE_CONTACT, payload: contact });
     };
 
     //filter contacts
+    const filterContacts = text => {
+        dispatch({ type: FILTER_CONTACTS, payload: text });
+    };
 
     //clear filter
+    const clearFilter = () => {
+        dispatch({ type: CLEAR_FILTER });
+    };
 
     return (
         <ContactContext.Provider
             value={{
                 contacts: state.contacts,
                 current: state.current,
+                filtered: state.filtered,
                 addContact,
                 deleteContact,
                 setCurrent,
                 clearCurrent,
                 updateContact,
+                filterContacts,
+                clearFilter,
             }}
         >
             {props.children}
