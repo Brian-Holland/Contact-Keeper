e1478c1455752339aa0397b6bfddc3bdd8ab3336
Brian <brian.holland.pr@gmail.com>
2020-05-24 21:57:31 -0700

    added loaduser and tokens

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
+import AlertContext from "../../context/alert/alertContext";
+import AuthContext from "../../context/auth/authContext";
 
-const Register = () => {
+const Register = props => {
+    const alertContext = useContext(AlertContext);
+    const authContext = useContext(AuthContext);
+
+    const { setAlert } = alertContext;
+
+    const { register, error, clearErrors, isAuthenticated } = authContext;
+
+    useEffect(() => {
+        if (isAuthenticated) {
+            props.history.push("/");
+        }
+        if (error === "User already exists") {
+            setAlert(error, "danger");
+            clearErrors();
+        }
+        //eslint-disable-next-line
+    }, [error, isAuthenticated, props.history]);
+
     const [user, setUser] = useState({
         name: "",
         email: "",
@@ -14,7 +34,17 @@ const Register = () => {
 
     const onSubmit = e => {
         e.preventDefault();
-        console.log("Register submit");
+        if (name === "" || email === "" || password === "") {
+            setAlert("Please enter all fields", "danger");
+        } else if (password !== password2) {
+            setAlert("Passwords do not match", "danger");
+        } else {
+            register({
+                name,
+                email,
+                password,
+            });
+        }
     };
     return (
         <div className="form-container">
@@ -29,6 +59,7 @@ const Register = () => {
                         name="name"
                         value={name}
                         onChange={onChange}
+                        required
                     />
                 </div>
                 <div className="form-group">
@@ -38,6 +69,7 @@ const Register = () => {
                         name="email"
                         value={email}
                         onChange={onChange}
+                        required
                     />
                 </div>
                 <div className="form-group">
@@ -47,6 +79,8 @@ const Register = () => {
                         name="password"
                         value={password}
                         onChange={onChange}
+                        required
+                        minLength="6"
                     />
                 </div>
                 <div className="form-group">
@@ -56,6 +90,8 @@ const Register = () => {
                         name="password2"
                         value={password2}
                         onChange={onChange}
+                        required
+                        minLength="6"
                     />
                 </div>
                 <input
--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,6 +1,8 @@
 import React, { useReducer } from "react";
+import axios from "axios";
 import AuthContext from "./authContext";
 import authReducer from "./authReducer";
+import setAuthToken from "../../utils/setAuthToken";
 import {
     REGISTER_SUCCESS,
     REGISTER_FAIL,
@@ -23,6 +25,48 @@ const AuthState = props => {
 
     const [state, dispatch] = useReducer(authReducer, initialState);
 
+    const loadUser = async () => {
+        if (localStorage.token) {
+            setAuthToken(localStorage.token);
+        }
+        try {
+            const res = await axios.get("/api/auth");
+
+            dispatch({ type: USER_LOADED, payload: res.data });
+        } catch (err) {
+            dispatch({ type: AUTH_ERROR });
+        }
+    };
+
+    const register = async formData => {
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        };
+        try {
+            const res = await axios.post("/api/users", formData, config);
+
+            dispatch({
+                type: REGISTER_SUCCESS,
+                payload: res.data,
+            });
+
+            loadUser();
+        } catch (err) {
+            dispatch({
+                type: REGISTER_FAIL,
+                payload: err.response.data.msg,
+            });
+        }
+    };
+
+    const login = () => console.log("login user");
+
+    const logout = () => console.log("logout user");
+
+    const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
+
     return (
         <AuthContext.Provider
             value={{
@@ -31,6 +75,11 @@ const AuthState = props => {
                 loading: state.loading,
                 user: state.user,
                 error: state.error,
+                register,
+                login,
+                logout,
+                clearErrors,
+                loadUser,
             }}
         >
             {props.children}
