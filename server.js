3a240c6619d765c0fc25a9622a277154d84202e6
Brian <brian.holland.pr@gmail.com>
2020-05-25 22:17:49 -0700

    fixed typo on server

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ app.use("/api/contacts", require("./routes/contacts"));
 if(process.env.NODE_ENV === "production"){
     app.use(express.static("client/build"));
 
-    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname," "client","build", "index.html" ")));
+    app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
 }
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
