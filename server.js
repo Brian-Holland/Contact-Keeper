a312f76296b0c243645d2a11184a1d9877b1da0d
Brian <brian.holland.pr@gmail.com>
2020-05-25 22:05:05 -0700

    fixed server file for deploy

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use("/api/contacts", require("./routes/contacts"));
 if(process.env.NODE_ENV === "production"){
     app.use(express.static("client/build"));
 
-    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "client","build", "index.html" )));
+    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname,"client/build")));
 }
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
