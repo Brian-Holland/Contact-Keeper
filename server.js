8db4d54dd98faa32a1673029cc4ebf0bd8c630f9
Brian <brian.holland.pr@gmail.com>
2020-05-25 21:24:01 -0700

    added prep for deployment on server and config files

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -13,8 +14,10 @@ app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 
-app.get("/", (req, res) => {
-    res.json({ msg: "Welcome to the ContactKeeper API" });
-});
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "client","build", "index.html" )));
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
